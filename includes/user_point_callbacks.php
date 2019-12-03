e415e04cbf81d0789d60276999303c0b8bbf6b61
Bartosz Kowalski <b.kowalski@visuality.pl>
2019-12-03 22:55:59 +0100

    Fix point adder

--- includes/user_point_callbacks.php
+++ includes/user_point_callbacks.php
@@ -7,8 +7,8 @@ function add_points($user_id, $points_to_add) {
 	$points_row = $wpdb->get_row( "SELECT * FROM $table_name WHERE user_id = $user_id AND date = CURDATE()" );
 	if (!!($points_row)) {
 		// update record
-		$new_points = $points_row->$points + $points_to_add;
-		$wpdb->update($table_name, array('points' => $new_points), array('ID' =>$points_row->ID));
+		$new_points = $points_row->points + $points_to_add;
+		$wpdb->update($table_name, array('points' => $new_points), array('id' =>$points_row->id));
 	} else {
 		// new record
 		$attributes = array(
