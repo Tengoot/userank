2e8711ba0c570fc4163c0d7eddf29191947f1a40
Bartosz Kowalski <b.kowalski@visuality.pl>
2019-12-04 20:42:37 +0100

    Change db structure

--- includes/points_table.php
+++ includes/points_table.php
@@ -3,29 +3,20 @@ function userank_database_install () {
     global $wpdb;
     global $userank_db_version;
 
-    $user_table_name = $wpdb->prefix . "userank_user_points";
-    $post_table_name = $wpdb->prefix . "userank_post_points";
+    $table_name = $wpdb->prefix . "userank_points";
     $charset_collate = $wpdb->get_charset_collate();
 
 	$sql = "
-CREATE TABLE $user_table_name (
+CREATE TABLE $table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
 		date date DEFAULT '0000-00-00' NOT NULL,
 		points mediumint(9) DEFAULT 0 NOT NULL,
-		user_id mediumint(9) NOT NULL,
+		rankable_id mediumint(9) NOT NULL,
+        rankable_type varchar(100) NOT NULL,
 		PRIMARY KEY  (id),
-        KEY  (user_id)
-) $charset_collate;
-CREATE TABLE $post_table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-		date date DEFAULT '0000-00-00' NOT NULL,
-		points mediumint(9) DEFAULT 0 NOT NULL,
-		post_id mediumint(9) NOT NULL,
-		PRIMARY KEY  (id),
-        KEY  (user_id)
+        KEY  (rankable_id, rankable_type)
 ) $charset_collate;
 ";
-
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 	dbDelta( $sql );
 
