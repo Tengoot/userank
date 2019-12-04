f43d2dd93f6325598203b08a71081d3133c94ddf
Bartosz Kowalski <b.kowalski@visuality.pl>
2019-12-04 20:46:55 +0100

    Adapt point actions to new structure

--- includes/user_point_callbacks.php
+++ includes/user_point_callbacks.php
@@ -1,10 +1,10 @@
 <?php
 
-function add_points($user_id, $points_to_add) {
+function add_points($id, $type, $points_to_add) {
 	global $wpdb;
-    $table_name = $wpdb->prefix . "userank_user_points";
+    $table_name = $wpdb->prefix . "userank_points";
 
-	$points_row = $wpdb->get_row( "SELECT * FROM $table_name WHERE user_id = $user_id AND date = CURDATE()" );
+	$points_row = $wpdb->get_row( "SELECT * FROM $table_name WHERE rankable_id = $id AND rankable_type = '$type' AND date = CURDATE()" );
 	if (!!($points_row)) {
 		// update record
 		$new_points = $points_row->points + $points_to_add;
@@ -14,7 +14,8 @@ function add_points($user_id, $points_to_add) {
 		$attributes = array(
 			'date' => date("Y-m-d"),
 			'points' => $points_to_add,
-			'user_id' => $user_id,
+			'rankable_id' => $id,
+			'rankable_type' => $type
 		);
 		$wpdb->insert($table_name, $attributes);
 	}
@@ -25,7 +26,7 @@ function points_for_post_creation($post_id, $post, $update) {
 	if ($update || !!(wp_is_post_revision($post_id)) || $user_id == 0 || is_super_admin($user_id))
 		return null;
 
-	add_points($user_id, 10);
+	add_points($user_id, 'user', 10);
 }
 
 add_action('save_post', 'points_for_post_creation', 11, 3);
